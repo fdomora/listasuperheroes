d71493c9ad60024b80885728fd13561523cb3ae4
Fernando Mora <fmora@utch.edu.mx>
2024-10-08 19:59:51 -0600

    se agrego imagen

--- app/src/main/java/com/example/listasuperheroes/DetailActivity.java
+++ app/src/main/java/com/example/listasuperheroes/DetailActivity.java
@@ -1,5 +1,6 @@
 package com.example.listasuperheroes;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -13,6 +14,7 @@ import com.example.listasuperheroes.databinding.ActivityDetailBinding;
 public class DetailActivity extends AppCompatActivity {
 
     public static final String SUPERHEROE_KEY = "superheroe";
+    public static final String BITMAP_KEY = "bitmap";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,10 +25,12 @@ public class DetailActivity extends AppCompatActivity {
 
         Bundle extras = getIntent().getExtras();
         Superheroe superheroe = extras.getParcelable(SUPERHEROE_KEY);
+        Bitmap bitmap = extras.getParcelable(BITMAP_KEY);
 
         binding.nombreHeroe.setText(superheroe.getNombre());
         binding.nombreRealText.setText(superheroe.getNomreal());
         binding.textView5.setText(superheroe.getBio());
         binding.ratingBar.setRating(superheroe.getPoder());
+        binding.imageView.setImageBitmap(bitmap);
     }
 }
--- app/src/main/java/com/example/listasuperheroes/MainActivity.java
+++ app/src/main/java/com/example/listasuperheroes/MainActivity.java
@@ -1,9 +1,15 @@
 package com.example.listasuperheroes;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 
 import androidx.activity.EdgeToEdge;
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -12,6 +18,9 @@ import androidx.core.view.WindowInsetsCompat;
 import com.example.listasuperheroes.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+    private Bitmap imgBitmap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,11 +36,28 @@ public class MainActivity extends AppCompatActivity {
             float poder = binding.poderBar.getRating();
             abrirDetailActivity(superheroeNom, nombreReal, biografia, poder);
         });
+
+        binding.heroeImagen.setOnClickListener(v -> {
+            camaraLauncher.launch(new Intent (MediaStore.ACTION_IMAGE_CAPTURE));
+        });
     }
+
+    ActivityResultLauncher<Intent> camaraLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+        @Override
+        public void onActivityResult(ActivityResult result) {
+            if(result.getResultCode() == RESULT_OK) {
+                Bundle extras = result.getData().getExtras();
+                imgBitmap = (Bitmap) extras.getParcelable("data");
+                binding.heroeImagen.setImageBitmap(imgBitmap);
+            }
+        }
+    });
+
     private void abrirDetailActivity(String superheroeNom, String nombreReal, String biografia, float poder){
         Superheroe superheroe = new Superheroe(superheroeNom, nombreReal, biografia,poder);
         Intent intent = new Intent(MainActivity.this, DetailActivity.class);
         intent.putExtra(DetailActivity.SUPERHEROE_KEY, superheroe);
+        intent.putExtra(DetailActivity.BITMAP_KEY, imgBitmap);
         startActivity(intent);
     }
 }
